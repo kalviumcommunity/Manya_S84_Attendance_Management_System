482385e11330aafc489b314ca6b016b0e29237a7
Manya <mayavi1319@gmail.com>
2025-08-06 09:39:16 +0530

    part02

---
+++ src/com/school/Course.java
@@ -0,0 +1,17 @@
+package com.school;
+
+public class Course {
+    int courseId;
+    String courseName;
+
+    public void setDetails(int id, String cName){
+        this.courseId = id;
+        this.courseName = cName;
+    }
+
+    public void displayDetails(){
+        System.out.println("Course ID: " + this.courseId + ", Course Name: " + this.courseName);
+    }
+
+
+}
--- src/com/school/Main.java
+++ src/com/school/Main.java
@@ -4,5 +4,33 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Welcome to the School Attendance System Project!");
         System.out.println("Session 1: Project Setup and Orientation Complete.");
+
+        Student[] students = new Student[2];
+        Course[] courses = new Course[2];
+
+        students[0] = new Student();
+        students[0].setDetails(1, "Bruce Banner");
+
+        students[1] = new Student();
+        students[1].setDetails(2, "Tony Stark");
+
+        courses[0] = new Course();
+        courses[0].setDetails(193, "Quantum Physics");
+
+        courses[1] = new Course();
+        courses[1].setDetails(274, "Astrophysics");
+
+        System.out.println("Student Details:");
+        for (Student student : students) {  
+            student.displayDetails();
+        }
+
+        System.out.println("Course Details:");
+        for (Course course : courses) {
+            course.displayDetails();
+        }
+
+
+
     }
 }
